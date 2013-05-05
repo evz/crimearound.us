0af4915a56e7b4f30b2aed43257a17871428b7f0
Eric van Zanten <eric@bahai.us>
2013-05-05 02:21:30 -0500

    Added some style

--- js/map.js
+++ js/map.js
@@ -24,19 +24,47 @@
         }
         map.setView([position.coords.latitude, position.coords.longitude], zoom);
         window.location.hash = zoom + ',' + position.coords.latitude + ',' + position.coords.longitude;
-        var then = moment().subtract('days', 14);
+        var then = moment().subtract('days', 9);
         var y = then.format('YYYY');
         var m = then.format('M');
         var d = then.format('D');
         var date_str = then.format('MM-DD-YYYY')
         var url = 'data/' + y + '/' + m + '/' + d + '.json';
+        var tpl = new EJS({url: 'js/views/dataTemplate.ejs'});
         $.getJSON(url, function(data){
-            addLayer(L.mapbox.markerLayer(data.geojson), date_str);
+            var marker_layer = L.mapbox.markerLayer(data.geojson).addTo(map);
+            var crime_template = new EJS({url: 'js/views/crimeTemplate.ejs'});
+            marker_layer.eachLayer(function(marker){
+                var props = marker.feature.properties;
+                var pop_content = crime_template.render(props);
+                console.log(pop_content);
+                marker.bindPopup(pop_content, {
+                    closeButton: true,
+                    minWidth: 320
+                })
+            })
+            data.date = date_str;
+            var html = tpl.render(data);
+            $('#overlay').html(html);
+            //filter_markers(marker_layer);
+            $('.filter').on('change', function(e){
+                filter_markers(marker_layer);
+            });
         });
     }
 
-    function addLayer(layer, name){
-        layer.addTo(map);
+    function filter_markers(marker_layer){
+        var on = [];
+        var checkboxes = $('.filter');
+        $.each(checkboxes, function(i, checkbox){
+            if($(checkbox).is(':checked')){
+                on.push($(checkbox).attr('name'));
+            }
+        });
+        marker_layer.setFilter(function(f){
+            return on.indexOf(f.properties['key']) !== -1;
+        });
+        return false;
     }
 
     function update_hash(e){
