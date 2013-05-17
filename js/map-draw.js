e5a803db7c052b19e66b007a6a88255b71015fd6
Eric van Zanten <eric@bahai.us>
2013-05-17 08:05:46 -0500

    Adding points back to map to complete the proof of concept. Google map tiles don't want to zoom, however, which probably means that the newer Leaflet API has changed enough to break the plugin that gets the google tile layers and configures them for Leaflet. Investigation required

--- js/map-draw.js
+++ js/map-draw.js
@@ -4,9 +4,8 @@
         window.onresize = function(event){
             resize_junk();
         }
-        //var map = L.map('map').fitBounds([[41.644286009999995, -87.94010087999999], [42.023134979999995, -87.52366115999999]])
-        //var googleLayer = new L.Google('ROADMAP');
-        //map.addLayer(googleLayer);
+        // var googleLayer = new L.Google('ROADMAP');
+        // var map = L.map('map', {layers:[googleLayer]}).fitBounds([[41.644286009999995, -87.94010087999999], [42.023134979999995, -87.52366115999999]])
         var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
         cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18}),
         map = new L.Map('map', {layers: [cloudmade]}).fitBounds([[41.644286009999995, -87.94010087999999], [42.023134979999995, -87.52366115999999]]);
@@ -30,7 +29,9 @@
             }
             query[query_params] = JSON.stringify(layer.toGeoJSON());
             $.when(get_results(query)).then(function(resp){
-                console.log(resp)
+                $.each(resp.results, function(i, result){
+                    var geojson = L.geoJson(result.location).addTo(map);
+                })
             }).fail(function(data){
                 console.log(data);
             })
