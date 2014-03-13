d166e030221c4c9e4fdf3e5b33a6116b1b5e3f68
Eric van Zanten <eric.vanzanten@gmail.com>
2014-03-13 13:08:32 -0500

    Removed District 00

--- js/analytics_lib.js
+++ js/analytics_lib.js
@@ -23,30 +23,30 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
 })();
 
 _trackClickEventWithGA = function (category, action, label) {
-	if (typeof(_gaq) != 'undefined')
+  if (typeof(_gaq) != 'undefined')
     _gaq.push(['_setAccount', analyticsTrackingCode]);
-		_gaq.push(['_trackEvent', category, action, label]);
+    _gaq.push(['_trackEvent', category, action, label]);
 };
 
 jQuery(function () {
 
-	jQuery('a').click(function () {
-		var $a = jQuery(this);
-		var href = $a.attr("href");
+  jQuery('a').click(function () {
+    var $a = jQuery(this);
+    var href = $a.attr("href");
 
-		//links going to outside sites
-		if (href.match(/^http/i) && !href.match(document.domain)) {
-			_trackClickEventWithGA("Outgoing", "Click", href);
-		}
+    //links going to outside sites
+    if (href.match(/^http/i) && !href.match(document.domain)) {
+      _trackClickEventWithGA("Outgoing", "Click", href);
+    }
 
-		//direct links to files
-		if (href.match(/\.(avi|css|doc|docx|exe|gif|js|jpg|mov|mp3|pdf|png|ppt|pptx|rar|txt|vsd|vxd|wma|wmv|xls|xlsx|zip)$/i)) {
-			_trackClickEventWithGA("Downloads", "Click", href);
-		}
+    //direct links to files
+    if (href.match(/\.(avi|css|doc|docx|exe|gif|js|jpg|mov|mp3|pdf|png|ppt|pptx|rar|txt|vsd|vxd|wma|wmv|xls|xlsx|zip)$/i)) {
+      _trackClickEventWithGA("Downloads", "Click", href);
+    }
 
-		//email links
-		if (href.match(/^mailto:/i)) {
-			_trackClickEventWithGA("Emails", "Click", href);
-		}
-	});
+    //email links
+    if (href.match(/^mailto:/i)) {
+      _trackClickEventWithGA("Emails", "Click", href);
+    }
+  });
 });
